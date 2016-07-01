d5e3cbc01b9f0f75ae5b17a91a9793fe5e2d650e
Jonas Östlund <uppfinnarjonas@gmail.com>
2016-07-01 07:56:20 +0200

    added compute index

--- src/epicea/utils.clj
+++ src/epicea/utils.clj
@@ -3,3 +3,14 @@
 (defn map-with-keys? [x ks]
   (if (map? x)
     (every? (fn [v] (contains? ks v)) (keys x))))
+
+(defn compute-matrix-index [sizes indices]
+  (assert (count sizes)
+          (count indices))
+  (loop [S sizes
+         I indices
+         i 0]
+    (if (empty? I)
+      i
+      (recur (pop S) (pop I) (+ (* i (last S)) (last I))))))
+      
--- test/epicea/utils_test.clj
+++ test/epicea/utils_test.clj
@@ -2,9 +2,16 @@
   (:require [clojure.test :refer :all]
             [epicea.utils :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
+(deftest map-with-keys-test
+  (testing "Testing map with keys"
     (is (map-with-keys? {:rulle 119} #{:rull :rulle}))
     (is (map-with-keys? {:rulle 119} #{:rulle}))
     (is (not (map-with-keys? {:rulle 119} #{:rull})))
     (is (not (map-with-keys? [:rulle] #{:rulle})))))
+
+(deftest matrix-index-test
+  (testing "Matrix index computation"
+    (is (= 0 (compute-matrix-index [3 4] [0 0])))
+    (is (= 1 (compute-matrix-index [3 4] [1 0])))
+    (is (= 3 (compute-matrix-index [3 4] [0 1])))
+    (is (= 4 (compute-matrix-index [3 4] [1 1])))))
