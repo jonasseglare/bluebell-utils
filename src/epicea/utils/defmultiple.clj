e23040211424c9fe9174768fcb4a2ab723f0a59a
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-04-30 21:46:17 +0200

    defmulti-all

--- src/epicea/utils/defmultiple.clj
+++ src/epicea/utils/defmultiple.clj
@@ -7,8 +7,6 @@
 (spec/def ::dispatch-value (constantly true))
 (spec/def ::whatever (constantly true))
 
-(defn defmultiple [& args]
-  (println "GOT ARGS: " args))
 
 (spec/def ::defmultiple #(= 'defmultiple %))
 
@@ -20,3 +18,28 @@
                          :name ::name
                          :dispatch-fun ::dispatch-fun
                          :methods (spec/* (spec/spec ::method)))) ;; explicit call to spec/spec.
+
+(defn make-method [entry]
+  {(:dispatch-value entry) 
+   (eval `(fn ~@(:function entry)))})
+
+(defn make-method-map [methods]
+  (reduce merge (map make-method methods)))
+
+(defn defmultiple-sub [x]
+  `(let [dispatch-fun# ~(eval (:dispatch-fun x))
+         method-map# ~(make-method-map (:methods x))]
+     (defn ~(:name x) [& args#]
+       (let [dv# (apply dispatch-fun# args#)
+             m# (get method-map# dv#)]
+         (if (nil? m#) 
+           (throw (RuntimeException. (str "No dispatch value for '" dv# "'")))
+           (apply m# args#))))))
+
+(defmacro defmultiple [& args]
+  (let [parsed (spec/conform ::defmultiple args)]
+    (if (= parsed ::spec/invalid)
+      (throw (RuntimeException. 
+              (with-out-str 
+                (spec/explain ::defmultiple args))))
+      (defmultiple-sub parsed))))
--- test/epicea/utils/defmultiple_test.clj
+++ test/epicea/utils/defmultiple_test.clj
@@ -8,3 +8,11 @@
           :dispatch-fun :op
           :methods [{:dispatch-value :katt, :function [['x] '(+ x 3)]}]}
          (spec/conform ::d/defmultiple '(evaluate :op (:katt [x] (+ x 3)))))))
+
+(defmultiple kattskit2 :op 
+  (:plus [k] (+ (:a k) (:b k)))
+  (:minus [k] (- (:a k) (:b k))))
+
+(deftest kattskit2-test
+  (is (= 7 (kattskit2 {:op :plus :a 3 :b 4})))
+  (is (= -1 (kattskit2 {:op :minus :a 3 :b 4}))))
