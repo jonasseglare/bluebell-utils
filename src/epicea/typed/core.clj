60d7f6d3d3dee4f34fb795fd25e67bf50bf804b7
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-05-10 22:18:05 +0200

    good so far

--- src/epicea/typed/core.clj
+++ src/epicea/typed/core.clj
@@ -4,9 +4,23 @@
             [epicea.utils.defmultiple :refer [defmultiple]]
             [epicea.utils.access :as access] :reload-all))
 
-(spec/def ::double (spec/or :type (partial = :double)
-                            :literal number?))
 (spec/def ::unspecified nil?)
+(spec/def ::bool boolean?)
+(spec/def ::float float?)
+(spec/def ::double double?)
+(spec/def ::long #(instance? java.lang.Long %))
+(spec/def ::int #(instance? java.lang.Integer %))
+(spec/def ::primitive (spec/or
+                       :bool (partial = :bool)
+                       :float (partial = :float)
+                       :long (partial = :long)
+                       :double (partial = :double)
+                       :int (partial = :int)))
+(spec/def ::dynamic (partial = :dynamic))
+
+(spec/def ::number (spec/or :type #(or (= % :number)
+                                       (= % :double))
+                            :literal number?))
 (spec/def ::count number?)
 (spec/def ::key keyword?)
 
@@ -29,7 +43,7 @@
 
 (spec/def ::vecdata (spec/cat
                      :type (partial = :vecdata)
-                     :values (spec/* ::sized-type)))
+                     :data (spec/* ::sized-type)))
 
 ;; Compile time tagging
 (spec/def ::tagged (spec/cat
@@ -37,10 +51,10 @@
                     :tag (constantly true)
                     :data ::sized-type))
 
-(spec/def ::sized-type (spec/or :double ::double
+(spec/def ::sized-type (spec/or :number ::number
                                 :record ::record
                                 :union ::union
-                                :unspecified ::unspecified
+                                :unspecified ::unspecified ;; <-- a primitive building block
                                 :tagged ::tagged
                                 :vec ::vec
                                 :vecdata ::vecdata))
@@ -50,15 +64,21 @@
                    :header (spec/? ::sized-type)
                    :data ::sized-type))
 
-;; How to specify one:
-;; (value [:record :a :double :b double] [3 [4 9]])
+(spec/def ::type (spec/or :sized-type ::sized-type
+                          :primitive ::primitive
+                          :array ::array
+                          :dynamic ::dynamic))
+                       
 
-(assert (spec/valid? ::double :double))
-(assert (spec/valid? ::union [:union :double :double]))
+;; How to specify one:
+;; (value [:record :a :number :b double] [3 [4 9]])
+
+(assert (spec/valid? ::number :number))
+(assert (spec/valid? ::union [:union :number :number]))
 (assert (spec/valid? ::record [:record 
-                               :a :double 
+                               :a :number 
                                :b [:vec nil 3]]))
-(assert (spec/valid? ::tagged [:tag 119 [:vec :double 3]]))
+(assert (spec/valid? ::tagged [:tag 119 [:vec :number 3]]))
 (assert (spec/valid? ::array [:array [:record :a nil :b nil]]))
 (assert (spec/valid? ::array [:array [:record :a nil :b nil] nil]))
 (assert (spec/valid? ::vecdata [:vecdata 3 4 5]))
@@ -82,17 +102,44 @@
 (def size-mul *)
 (def size-max max)
 
+
+;;;; Compute the size of a ::sized-type
 (defmultiple compute-size get-pair-tag
   (:vec [x] (let [value (get-pair-value x)]
               (size-mul (:count value) (compute-size (:type value)))))
-  (:double [x] 1)
+  (:number [x] 1)
   (:tagged [x] (compute-size (:data (get-pair-value x))))
   (:union [x] (reduce size-max (map compute-size (-> x get-pair-value :alts))))
   (:record [x] (reduce size-add (map (comp compute-size :value) 
-                                    (-> x get-pair-value :fields)))))
+                                    (-> x get-pair-value :fields))))
+  (:unspecified [x] nil)
+  (:vecdata [x] (reduce size-add (map compute-size (-> x get-pair-value :data)))))
+
   
-(assert (= 1 (compute-size (spec/conform ::sized-type [:tag 119 :double]))))
+(assert (= 1 (compute-size (spec/conform ::sized-type [:tag 119 :number]))))
 (assert (= 1 (compute-size (spec/conform ::sized-type 3))))
-(assert (= 3 (compute-size (spec/conform ::sized-type [:vec :double 3]))))
-(assert (= 2 (compute-size (spec/conform ::sized-type [:union :double [:vec :double 2]]))))
-(assert (= 2 (compute-size (spec/conform ::sized-type [:record :a :double :b :double]))))
+(assert (= 3 (compute-size (spec/conform ::sized-type [:vec :number 3]))))
+(assert (= 2 (compute-size (spec/conform ::sized-type [:union :number [:vec :number 2]]))))
+(assert (= 2 (compute-size (spec/conform ::sized-type [:record :a :number :b :number]))))
+(assert (= 3 (compute-size (spec/conform 
+                            ::sized-type 
+                            [:record :a :number :b :number :c :number]))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; Grammar
+(spec/def ::arg-name symbol?)
+
+(spec/def ::arg-decl (spec/cat
+                    :type ::type
+                    :name ::arg-name))
+
+(spec/def ::arg-list (spec/* ::arg-decl))
+
+(spec/def ::statement (constantly true))
+
+(spec/def ::typed-fun (spec/cat
+                       :return-type ::type
+                       :arg-list (spec/spec ::arg-list)
+                       :body (spec/* ::statement)))
+(assert (not= 
+         ::spec/invalid 
+         (spec/conform ::typed-fun [nil [:double 'a :double 'b] :a :b :c])))
