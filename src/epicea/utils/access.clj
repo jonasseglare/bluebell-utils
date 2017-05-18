ef874efb6fddf35ba503384f5379514551b183b0
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-05-18 21:30:19 +0200

    major refactoring

--- src/epicea/utils/access.clj
+++ src/epicea/utils/access.clj
@@ -2,249 +2,80 @@
   (:require [epicea.utils.debug :as debug]
             [epicea.utils.core :as core]
             [epicea.utils.optional :refer [optional]]
-            [clojure.spec :as spec]))
+            [clojure.spec :as spec]
+            [clojure.pprint :as pprint]))
 
-(defn accessor? [x]
-  (and (map? x)
-       (every? #(contains? x %) [:set :get :has?])))
-
-;;;;; Standard accessors
-
-(def default-obj {})
-(def default-opts {})
-(def default-map-opts (merge default-opts {:valid-value? (constantly true)
-                                           :make-default-parent 
-                                           (fn [x] 
-                                             (if (nil? x) {} x))
-                                           :valid-parent? map?}))
-
-(defn valid-parent? [x accessor]
-  (if (contains? accessor :valid-parent?)
-    ((:valid-parent? accessor) x)
-    true))
-
-(defn get-default-parent [accessor]
-  (:default-parent accessor))
-
-(defn make-default-parent [accessor x]
-  (if (contains? accessor :make-default-parent) 
-    ((:make-default-parent accessor) x)
-    (if (valid-parent? x accessor)
+(defn shorten [x]
+  (let [n 100]
+    (if (< (count x) n)
       x
-      (get-default-parent accessor))))
+      (str (subs x 0 n) "..."))))
+
+(defn data-to-str [x]
+  (shorten (with-out-str (pprint/pprint x))))
+
+(defn accessor-error [what accessor root value]
+  (core/common-error 
+   (str "ACCESSOR ERROR"
+        "\n  In the context " what
+        "\n  With accessor  " (data-to-str accessor)
+        "\n  On root value  " (data-to-str root)
+        "\n  For some value " (data-to-str value))))
+
+(def base-opts {:info nil
+                :valid-base? (constantly true)
+                :default-base {}
+                :default-value nil
+                :valid-value? nil})
+
+(defn map-methods [key]
+  {:has? #(contains? % key)
+   :get #(get % key)
+   :set (fn [obj x] (assoc obj key x))})
+
+(defn default-map-opts [key]
+  {:valid-base? map?
+   :info {:type :map-accessor :key key}})
+
+(defn apply-decorator [accessor [key dec]]
+  (if (contains? accessor key)
+    accessor
+    (assoc accessor key (dec accessor))))
 
 
+(defn make-validate-base [accessor]
+  (let [v? (:valid-base? accessor)]
+    (fn [x]
+      (if (v? x) 
+        x 
+        (accessor-error "validate-base" 
+                        accessor x nil)))))
 
-(defn make-default [obj accessor]
-  (if (contains? accessor :make-default)
-    ((:make-default accessor) obj)))
+(defn make-validate-has [accessor]
+  (let [h? (:has? accessor)]
+    (fn [x]
+      (if (h? x)
+        x
+        (accessor-error "missing value"
+                        accessor x nil)))))
 
+(def decorators [[:validate-base make-validate-base]
+                 [:validate-has make-validate-has]])
 
+(defn decorate-accessor [a]
+  (reduce apply-decorator
+          a
+          decorators))
 
-;; Assumptions:
-;;  - Always a valid input object.
-
-(defn check-validity [type-of-action key base-opts source value]
-  (if (not ((:valid-value? base-opts) value))
-    (throw (RuntimeException. 
-            (str "Invalid value obtained using '" type-of-action
-                 "' with key '" key "' on source '" 
-                 source "' getting value '"
-                 value "'")))))
-
-(defn check-parent 
-  ([action key opts y x]
-   (if (not ((:valid-parent? opts) y))
-     (throw (RuntimeException. 
-             (str "INVALID parent '" y "' constructed from '" x "' for key '" key "'")))))
-  ([action key opts y]
-   (if (not ((:valid-parent? opts) y))
-     (throw (RuntimeException. 
-             (str "INVALID parent '" y "' for key '" key "'"))))))
-   
-
-(defn ensure-parent [action key opts x]
-  (let [y (make-default-parent opts x)]
-    (check-parent action key opts y x)
-    y))
-
+;;;;;; Main map creator
 (defn map-accessor 
-  ([key map-opts]
-   (let [base-opts (merge default-map-opts map-opts)]
-     (merge base-opts
-          {:get (fn [src] 
-                  (debug/dout src)
-                  (let [x (get src key)]
-                    (check-validity :get key base-opts src x)
-                    x))
-           :has? (fn [obj]
-                   (check-parent :has? key base-opts obj)
-                   (contains? obj key))
-           :set (fn [obj x] 
-                  (check-validity :set key base-opts obj x)
-                  (assoc (ensure-parent :set key base-opts obj) key x))})))
+  ([key extra-opts]
+   (decorate-accessor
+    (merge base-opts 
+           (default-map-opts key)
+           extra-opts 
+           (map-methods key))))
   ([key] (map-accessor key {})))
 
-(def default-vector-opts default-opts)
 
-(defn vector-accessor 
-  ([index opts]
-   (merge default-vector-opts 
-          opts
-          (let [valid? #(and (vector? %) (< index (count %)))]
-            (assert (valid? (:default-parent opts)))
-            {:get #(nth % index)
-             :valid-parent? valid?
-             :has? valid?
-             :set (fn [obj x] 
-                    (assert (valid? obj))
-                    (assoc obj index x))}))))
-
-;; Wraps an element in a vector
-(def vec1-accessor (merge default-opts {:get (fn [x] [x]) 
-                                        :set (fn [x y] (first y))
-                                        :has? (constantly true)}))
-          
-;;;;; Utilities
-(defn getx [obj accessor]
-  ((:get accessor) obj))
-
-(defn setx [obj accessor x]
-  ((:set accessor) obj x))
-
-(defn updatex [obj accessor f]
-  (if ((:has? accessor) obj)
-    ((:set accessor) obj (f ((:get accessor) obj)))
-    obj))
-
-(defn has? [obj accessor]
-  ((:has? accessor) obj))
-
-(defn getx-or-default [obj accessor]
-  (assert (accessor? accessor))
-  (if (has? obj accessor) 
-    (getx obj accessor)
-    (make-default obj accessor)))
-
-(defn getx-optional [obj accessor]
-  (if ((:has? accessor) obj)
-    (optional ((:get accessor) obj))
-    (optional)))
-
-
-(defn er [which]
-  (fn [accessor]
-    (assert (accessor? accessor))
-    (get accessor which)))
-
-(def hasser? (er :has?))
-
-(def getter (er :get))
-
-(def setter (er :set))
-
-(defn updater [accessor]
-  (fn [obj f] (updatex obj accessor f)))
-
-(defn parts [accessor]
-  (if (contains? accessor :parts)
-    (:parts accessor) 
-    [accessor]))
-
-(defn complex? [accessor]
-  (contains? accessor :parts))
-
-(defn simple? [accessor]
-  (not (simple? accessor)))
-
-(defn size [x]
-  (count (parts x)))
-
-;;;;; Composition
-
-(defn compose-getx [a b]
-  (fn [x] (-> x a b)))
-
-(defn compose-has? [a b]
-  (fn [x] (and ((:has? a) x) 
-               ((:has? b) ((:get a) x)))))
-
-(defn compose-setx [a b]
-  (fn [obj x] 
-    (let [subobj (getx-or-default obj a)]
-      (setx obj a (setx subobj b x)))))
-
-(defn catparts [a b]
-  (vec (concat a b)))
-
-(defn compose2 [a b]
-  {:parts (catparts (parts a) (parts b))
-   :default-parent (:default-parent a)
-   :make-default (:make-default b)
-   :get (compose-getx (:get a) (:get b))
-   :has? (compose-has? a b)
-   :set (compose-setx a b)})
-
-(defn compose [& args]
-  (reduce compose2 args))
-
-;;;;; Slicing, etc
-(defn slice [accessor from to]
-  (reduce compose (subvec (parts accessor) from to)))
-
-(defn diff-ancestors [a b]
-  (loop [result []
-         ap (parts a)
-         bp (parts b)]
-    (if (or (empty? ap) 
-            (empty? bp)
-            (not= (first ap) (first bp)))
-      {:common result
-       :a ap
-       :b bp}
-      (recur (conj result (first ap))
-             (rest ap)
-             (rest bp)))))
-
-;;;;;; Constructor
-
-(spec/def ::build-arg (spec/cat :accessor accessor?
-                                :value (constantly true)))
-(spec/def ::build-args (spec/* ::build-arg))
-
-(defn build [& args]
-  (let [parsed (spec/conform ::build-args args)]
-    (if (= parsed ::spec/invalid)
-      (core/common-error 
-       "Invalid build args: " 
-       (spec/explain-str ::build-args args))
-      (reduce
-       (fn [obj build-arg]
-         (setx (:accessor build-arg)
-               obj
-               (:value build-arg)))
-       nil
-       parsed))))
-      
-
-
-(defn constructor 
-  ([init accessors]
-   (fn [& args]
-     (reduce
-      (fn [obj f]
-        (f obj))
-      init
-      (map (fn [accessor arg]
-             (fn [obj] 
-               ((:set accessor) 
-                obj arg)))
-           accessors args))))
-  ([accessors]
-   (constructor (:default-parent (first accessors))
-                accessors)))
-
-
-      
-            
-            
+(def q (map-accessor :q))
--- test/epicea/utils/access_test.clj
+++ test/epicea/utils/access_test.clj
@@ -3,84 +3,18 @@
             [epicea.utils.access :refer :all]
             [epicea.utils.optional :refer [optional]]))
 
-(def k (map-accessor :k))
-(def w (map-accessor :w))
+(def k (map-accessor :k {}))
 
-(def kw (compose k w))
+(deftest basic-map-access
+  (is (= 3 ((:get k) {:k 3})))
+  (is (= {:k 9} ((:set k) {} 9)))
+  (is ((:has? k) {:k 3}))
+  (is (not ((:has? k) {:p 3}))))
 
-(def set-k (setter k))
-(def get-k (getter k))
-(def update-k (updater k))
-(def update-kw (updater kw))
+(deftest base-validator
+  (is (thrown? Throwable ((:validate-base k) [])))
+  (is (= {:r 4} ((:validate-base k) {:r 4}))))
 
-(deftest access-test
-  (is (accessor? (map-accessor :k)))
-  (is (not (accessor? {})))
-  (is (= {:k 9} (setx {} k 9)))
-  (is (not (has? {} k)))
-  (is (has? (setx {} k 9) k))
-  (is (= 9 (getx {:k 9} k)))
-  (is (= 9 (get-k {:k 9}))))
-
-(deftest compose-test
-  (is (has? {:k {:w 3}} kw))
-  (is (= 3 (getx {:k {:w 3}} kw)))
-  (is (= {:r 3 :k {:w 4}} (setx {:r 3} kw 4)))
-
-  (is (= {:k 4} (update-k {:k 3} inc)))
-  (is (= {:k {:w 4}} (update-kw {:k {:w 3}} inc)))
-  (is (= 2 (size kw)))
-  (is (= 1 (size k)))
-  (is (not (empty? (:common (diff-ancestors kw k)))))
-  (is (empty? (:common (diff-ancestors kw w)))))
-
-(def default-vec [nil nil nil nil])
-(def sec (vector-accessor 1 {:default-parent default-vec}))
-
-
-(deftest vector-test
-  (is (= [nil 3 nil nil] (setx default-vec sec 3)))
-  (is (= [nil 4 nil nil] (updatex [nil 3 nil nil] sec inc))))
-
-  (def x (map-accessor :x))
-(def y (map-accessor :y))
-(def make-xy (constructor {} [x y]))
-
-
-(deftest constructor-test
-  (is (= {:x 3 :y 4} (make-xy 3 4))))
-
-
-  (def empty-xy2 [nil nil])
-(def x2 (vector-accessor 0 {:default-parent empty-xy2}))
-(def y2 (vector-accessor 1 {:default-parent empty-xy2}))
-(def make-xy2 (constructor [x2 y2]))
-
-(deftest constructor-test2
-  (is (= [9 20] (make-xy2 9 20))))
-
-
-(def even-access
-  {:id :even-number
-   :default-parent 0
-   :get identity
-   :has? even?
-   :set (fn [a b] b)})
-
-(deftest even-access-test
-  (is (= 2 (getx 2 even-access)))
-  (is (= (optional 2) (getx-optional 2 even-access)))
-  (is (= (optional) (getx-optional 3 even-access))))
-
-(def whiskers2 (map-accessor :whiskers {:default-parent {:type :cat :whiskers []}}))
-(def whiskers3 (map-accessor :whiskers {:make-default (fn [_] [1 2 3])}))
-
-
-(deftest cat-test
-  (is (= [] (getx-or-default {} whiskers2)))
-  (is (= [1 2 3] (getx-or-default {} whiskers3))))
-
-(comment
-(deftest vec1-accessor-test
-  (is (= 4 (updatex vec1-accessor 3 (fn [k] (map inc k))))))
-)
+(deftest validate-has-test
+  (is (thrown? Throwable ((:validate-has k) {:r 9})))
+  (is (= {:k 111} ((:validate-has k) {:k 111}))))
