2f6873eeaf3edf1ad330df9592ad3c7ff2204703
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-06-06 20:56:32 +0200

    good so far

--- src/epicea/utils/egraph.clj
+++ src/epicea/utils/egraph.clj
@@ -1,5 +1,7 @@
 (ns epicea.utils.egraph
-  (:require [clojure.spec :as spec]))
+  (:require [clojure.spec :as spec]
+            [epicea.utils.access :as access]))
+
 
 (spec/def ::unique-tag (partial = ::node))
 
@@ -7,3 +9,25 @@
 
 (defn node? [x]
   (spec/valid? ::node x))
+
+(def empty-node {:unique-tag ::node})
+(def settings {:default-base empty-node})
+
+(def -simple? (access/key-accessor :simple? (merge settings {:valid-value? boolean?})))
+(def -type (access/key-accessor :type (merge settings {:valid-value? keyword?})))
+(def -expr (access/key-accessor :expr settings))
+(def -args (access/key-accessor :args (merge settings {:valid-value? vector?})))
+(def -make (access/key-accessor :make (merge settings {:valid-value? fn?})))
+
+(defn simple-expr? [x]
+  (or (keyword? x)
+      (symbol? x)
+      (number? x)
+      (string? x)
+      (boolean? x)))
+      
+
+(defn primitive-expr [tp value]
+  (access/build -type tp
+                -expr value
+                -simple? (simple-expr? value)))
--- test/epicea/utils/egraph_test.clj
+++ test/epicea/utils/egraph_test.clj
@@ -4,4 +4,6 @@
 
 (deftest egraph-test-node?
   (is (node? {:unique-tag ::egraph/node :kattskit 9}))
-  (is (not (node? {:unique-tag 9}))))
+  (is (node? empty-node))
+  (is (not (node? {:unique-tag 9})))
+  (is (node? (primitive-expr :double 9))))
