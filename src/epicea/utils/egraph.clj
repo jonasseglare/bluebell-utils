9623b568c8bfd1990171b05277dea7749d1bf2a8
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-06-06 21:43:42 +0200

    bra

--- src/epicea/utils/egraph.clj
+++ src/epicea/utils/egraph.clj
@@ -13,21 +13,67 @@
 (def empty-node {:unique-tag ::node})
 (def settings {:default-base empty-node})
 
+(defn key? [x]
+  (or (symbol? x)
+      (keyword? x)))
+
+(defn valid-args? [x]
+  (and (coll? x)
+       (every? #(or (node? %)
+                    (key? %))
+               x)))
+
 (def -simple? (access/key-accessor :simple? (merge settings {:valid-value? boolean?})))
 (def -type (access/key-accessor :type (merge settings {:valid-value? keyword?})))
 (def -expr (access/key-accessor :expr settings))
-(def -args (access/key-accessor :args (merge settings {:valid-value? vector?})))
+(def -args (access/key-accessor :args (merge settings {:valid-value? valid-args?})))
 (def -make (access/key-accessor :make (merge settings {:valid-value? fn?})))
 
+(defn node-of-type? 
+  ([type node]
+   (= type (access/get node -type)))
+  ([type] #(node-of-type? type %)))
+    
 (defn simple-expr? [x]
   (or (keyword? x)
       (symbol? x)
       (number? x)
       (string? x)
       (boolean? x)))
-      
 
 (defn primitive-expr [tp value]
   (access/build -type tp
                 -expr value
                 -simple? (simple-expr? value)))
+
+(declare add-subexpr)
+
+(defn add-simple-subexpr [dst key x]
+  (assert (node? x))
+  (assert (key? key))
+  (assert (map? dst))
+  (assoc dst key x))
+
+(defn add-complex-subexpr [dst key x]
+  (let [args (access/get x -args)
+        named-args (map (fn [x] [(gensym) x]) args)]
+    (assoc (reduce (fn [dst [k v]]
+                     (add-subexpr dst k v))
+                   dst named-args)
+           key (access/set x -args (map first named-args)))))
+
+(defn add-subexpr [dst key x]           
+  (if (access/has? x -args)
+    (add-complex-subexpr dst key x)
+    (add-simple-subexpr dst key x)))
+
+;; (defn add-node-subexpressions [dst x]
+;;   (add-args dst (access/get x -args))
+
+;; (defn add-subexpressions [dst x]
+;;   (if (node? x)
+;;     (add-node-subexpressions dst x)
+;;     (add-other
+
+;; (defn make-expression-map [x]
+;;   (reduce add-subexpressions {} x))
--- test/epicea/utils/egraph_test.clj
+++ test/epicea/utils/egraph_test.clj
@@ -1,7 +1,8 @@
 (ns epicea.utils.egraph-test
   (:require [clojure.test :refer :all]
             [epicea.utils.access :as access]
-            [epicea.utils.egraph :refer :all :as egraph]))
+            [epicea.utils.egraph :refer :all :as egraph]
+            [clojure.spec :as spec]))
 
 (deftest egraph-test-node?
   (is (node? {:unique-tag ::egraph/node :kattskit 9}))
@@ -12,3 +13,46 @@
         b (primitive-expr :double '(+ 3 4))]
     (is (access/get a -simple?))
     (is (not (access/get b -simple?)))))
+
+(defn dnum [x]
+  (primitive-expr :double x))
+
+(defn make-dnum-op [maker]
+  (fn [& args]
+    (access/build -type :double
+                  -simple? false
+                  -make maker
+                  -args args)))
+
+(def test-add (make-dnum-op (fn [args] `(+ ~@args))))
+(def test-mul (make-dnum-op (fn [args] `(* ~@args))))
+(def test-div (make-dnum-op (fn [args] `(/ ~@args))))
+(def test-sqrt (make-dnum-op (fn [args] `(sqrt ~@args))))
+
+(spec/def ::double-vector (spec/coll-of (node-of-type? :double)))
+
+(deftest is-double-vector-test
+  (is (spec/valid? ::double-vector [(dnum 9) (dnum 3)])))
+
+(defn normalize [v]
+  (assert (spec/valid? ::double-vector v))
+  (let [sum-squares (reduce test-add (map (fn [x] (test-mul x x)) v))
+        norm (test-sqrt sum-squares)]
+    (map (fn [x] (test-div x norm)) v)))
+
+(deftest normalization-test
+  (is (every? node? (normalize [(dnum 3) (dnum 4)]))))
+
+(deftest expr-test
+  (is (node? (dnum 9)))
+  (is (not (node? 9)))
+  (is (node? (test-add (dnum 9) (dnum 3)))))
+
+(deftest expression-map-test
+  (is (map? (add-simple-subexpr {} :mjao (dnum 9))))
+  (let [expanded (add-subexpr {} :kattskit (test-add (dnum 3) (dnum 4)))]
+    (assert (is (map? expanded)))
+    (assert (every? (fn [[k v]]
+                      (and (key? k)
+                           (node? v))) expanded))
+    nil))
