7decd14b334435eaf6fb075bfe4fddd91b988bc5
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-06-08 08:04:18 +0200

    project

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject epicea/utils "0.1.0-SNAPSHOT"
+(defproject nettle/utils "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
