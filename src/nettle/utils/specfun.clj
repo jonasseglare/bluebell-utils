2f3ac69f73d4dd399c46adb4704e5838066a095f
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-06-11 14:57:53 +0200

    good

--- src/nettle/utils/specfun.clj
+++ src/nettle/utils/specfun.clj
@@ -8,10 +8,11 @@
 
 (spec/def ::spec (constantly true))
 (spec/def ::argsym symbol?)
+(spec/def ::arglist1 (spec/spec (spec/cat :arg ::argsym)))
 (spec/def ::bodyform (constantly true))
 
 (spec/def ::def (spec/cat :spec ::spec
-                          :argsym ::argsym
+                          :args ::arglist1
                           :body (spec/* ::bodyform)))
 
 (spec/def ::defs (spec/* (spec/spec ::def)))
@@ -21,7 +22,9 @@
 
 (defn defs-to-map [defs]
   (into {} (map (fn [m]
-                  {(:spec m) `(fn [~(:argsym m)] ~@(:body m))})
+                  {(:spec m) 
+                   `(fn [~(-> m :args :arg)] 
+                      ~@(:body m))})
                 defs)))
 
 (defn defspecfun-sub [x]
@@ -29,10 +32,33 @@
               (fn [current#]
                 (merge (or current# {})
                        ~(defs-to-map (:defs x))))))))
-                      
+
+(defn find-confs [key args]
+  (filter
+   (complement nil?)
+   (map
+    (fn [[sp impl]]
+      (let [y (spec/conform sp args)]
+        (if (not= y ::spec/invalid)
+          [sp y impl])))
+    (get (deref funs) key))))
+   
+
+(defn evaluate-specfun [key]
+  (fn [& args]
+    (let [confs (find-confs key args)]
+      (cond
+        (empty? confs) (utils/common-error "No implementation found for " key " and " args)
+        (= 1 (count confs)) (let [[_ parsed-args impl] (first confs)]
+                              (impl parsed-args))
+        :default (utils/common-error "Conformance ambiguity for " 
+                               key " and " args ": " 
+                               (apply str (map first confs)))))))
 
 (defmacro defspecfun [& args]
   (let [x (spec/conform ::defspecfun args)]
     (if (= x ::spec/invalid)
       (utils/common-error (spec/explain-str ::defspecfun args))
-      (defspecfun-sub x))))
+      `(do
+         ~(defspecfun-sub x)
+         (def ~(:name x) (evaluate-specfun (quote ~(:name x))))))))
--- test/nettle/utils/specfun_test.clj
+++ test/nettle/utils/specfun_test.clj
@@ -4,6 +4,20 @@
             [clojure.spec :as spec]))
 
 (deftest various-spec
-  (is (spec/valid? ::specfun/def '(my-spec name (+ 1 2) (+ 4 5))))
-  (is (spec/valid? ::specfun/defs '((a avar (+ 3 4) (+ 5 6))
-                                    (b bvar (+ 3 4) (+ 5 6))))))
+  (is (spec/valid? ::specfun/def '(my-spec [name] (+ 1 2) (+ 4 5))))
+  (is (spec/valid? ::specfun/defs '((a [avar] (+ 3 4) (+ 5 6))
+                                    (b [bvar] (+ 3 4) (+ 5 6))))))
+
+(spec/def ::numbers (spec/* number?))
+(spec/def ::strings (spec/* string?))
+(spec/def ::vectors (spec/* vector?))
+
+(defspecfun add-any 
+  (::numbers [x] (apply + x))
+  (::strings [x] (apply str x))
+  (::vectors [x] (reduce into [] x)))
+
+(deftest add-any-test
+  (is (= 7 (add-any 3 4)))
+  (is (= "aabbb" (add-any "aa" "bbb")))
+  (is (= [:a 3 4] (add-any [:a 3] [4]))))
