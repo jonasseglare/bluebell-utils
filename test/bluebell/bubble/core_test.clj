4ca2c3a8382a212d2ba7811f7d7e484786b17f4f
Jonas Östlund <uppfinnarjonas@gmail.com>
2017-11-11 09:17:12 +0100

    great stuff

--- test/bluebell/bubble/core_test.clj
+++ test/bluebell/bubble/core_test.clj
@@ -2,7 +2,9 @@
   (:require [clojure.test :refer :all]
             [bluebell.bubble.core :refer :all]))
 
+(def my-add (protect-fn +))
 
 (deftest bubble-test
   (is (bubble? (bubble 3)))
-  (is (not (bubble? 3))))
+  (is (not (bubble? 3)))
+  (is (= 7 (my-add 3 4))))
