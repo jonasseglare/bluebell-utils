fdf5965a9724ff2c72b9102d30678de21034b93e
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-05-10 10:26:54 +0200

    add it

---
+++ test/bluebell/utils/pareto_test.clj
@@ -0,0 +1,38 @@
+(ns bluebell.utils.pareto-test
+  (:require [bluebell.utils.pareto :refer :all]
+            [clojure.test :refer :all]))
+
+(def empty-frontier (frontier (fn [[a b] [x y]] (and (< a x)
+                                                     (< b y)))))
+
+(deftest reduction-test
+  (let [reds (map :elements
+                  (reductions insert
+                              empty-frontier
+                              [[10 10]
+                               [20 5]
+                               [30 2]
+                               [29 1]
+                               [4 4]
+                               [5 5]]))]
+    (is (=
+
+         reds
+
+         [
+          []
+
+          [[10 10]]
+
+          [[10 10] [20 5]]
+
+          [[10 10] [20 5] [30 2]]
+
+          [[10 10] [20 5] [29 1]]
+
+          [[29 1] [4 4]]
+          
+          [[29 1] [4 4]]
+
+          ]))))
+
