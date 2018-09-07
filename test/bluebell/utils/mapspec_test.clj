0c00a8319bd902a9ba65a457b7b8ba9281a7d067
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-09-07 10:57:24 +0200

    good so far

---
+++ test/bluebell/utils/mapspec_test.clj
@@ -0,0 +1,15 @@
+(ns bluebell.utils.mapspec-test
+  (:require [bluebell.utils.mapspec :refer :all]
+            [clojure.test :refer :all] :reload))
+
+;; (specify Kattskit)
+
+;; (deftest kattskit-test
+;;   (is (= {} (Kattskit {})))
+  
+;;   )
+
+;; (def k (field render-settings))
+
+;; (specify PlotState
+;;   k)
