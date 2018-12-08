f681d9f5218e374a8c4a7bfda8f796b015d09441
Jonas Östlund <uppfinnarjonas@gmail.com>
2018-12-08 10:58:09 +0100

    Show time it takes to rebuild EBMD

--- src/java/bluebell/utils/ebmd/Registry.java
+++ src/java/bluebell/utils/ebmd/Registry.java
@@ -266,11 +266,17 @@ public class Registry {
     }
 
     public void rebuild() {
+        long start = System.currentTimeMillis();
         // Set this at the top, so that recursive call will see it as being rebuilt.
         _rebuiltAt = _mutationCounter;
 
         rebuildArgSpecs();
         rebuildPromotionPaths();
+        if (_settings.dispRebuildTime) {
+            System.out.println(
+                "Rebuilt EBMD registry in " + (System.currentTimeMillis() - start) 
+                + " milliseconds");
+        }
     }
 
     public boolean rebuildIfNeeded() {
--- src/java/bluebell/utils/ebmd/Settings.java
+++ src/java/bluebell/utils/ebmd/Settings.java
@@ -2,6 +2,7 @@ package bluebell.utils.ebmd;
 
 public class Settings {
     public boolean check = true;
+    public boolean dispRebuildTime = true;
     
     public Settings debug() {
         check = true;
