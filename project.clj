a76ac5a20242919bab4a4c0d69b8b02ecb433cf2
Jonas Östlund <uppfinnarjonas@gmail.com>
2019-03-08 12:21:20 +0100

    Release new version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bluebell/utils "0.1.9-SNAPSHOT"
+(defproject bluebell/utils "0.1.10-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
